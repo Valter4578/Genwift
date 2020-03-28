9227f73fa6ed8b2e876e74d1d4cdd28d70c356ea
Максим Алексеев <mrvalter566@gmail.com>
2020-03-28 23:27:28 +0700

    Code inpruvment

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	presenterBody := "class " + moduleName + "Presenter: " + moduleName + "Output { \n<#T##Presenter's code#> \n}"
+	presenterBody := "class " + moduleName + "Presenter: " + moduleName + "Output {\n	weak var view:" + moduleName + "Input? \n	<#T##Presenter's code#> \n}\ninit(view: " + moduleName + ") {\n	self.view = view \n}"
 	fmt.Println(presenterBody)
 	_, err = presenter.WriteString(presenterBody)
 	if err != nil {
@@ -32,7 +32,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	vcBody := "class " + moduleName + "ViewController: " + moduleName + "Input { \n<#T##Presenter's code#> \n}"
+	vcBody := "class " + moduleName + "ViewController: " + moduleName + "Input { \n	<#T##Presenter's code#> \n}"
 	fmt.Println(vcBody)
 	_, err = vc.WriteString(vcBody)
 	if err != nil {
@@ -44,7 +44,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	inputBody := "protocol " + moduleName + "Input" + " { \n<#T##Input's protocol code#> \n}"
+	inputBody := "protocol " + moduleName + "Input" + " { \n	<#T##Input's protocol code#> \n}"
 	fmt.Println(inputBody)
 	_, err = input.WriteString(inputBody)
 	if err != nil {
@@ -56,7 +56,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	outputBody := "protocol " + moduleName + "Output" + " { \n<#T##Output's protocol code#> \n}"
+	outputBody := "protocol " + moduleName + "Output" + " { \n	<#T##Output's protocol code#> \n}"
 	fmt.Println(inputBody)
 	_, err = output.WriteString(outputBody)
 	if err != nil {
